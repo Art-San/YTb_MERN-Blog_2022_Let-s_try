49481774f4f7aa132761bfa66aa37a4f0d4229ff
Aleksander <asava@sibmail.com>
2023-03-10 20:17:50 +0700

    add peges

---
+++ 0.dop/2.client.jsx
@@ -0,0 +1,50 @@
+1. npx create-react-app .
+2. Удалили все лишние
+3. Подключили шрифт гугл //https://fonts.google.com/specimen/Poppins?query=poppin
+  1. Одну часть в indexedDB.html
+{/* <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"></link> */}
+ 2. Другую часть в index.css
+//  font-family: 'Poppins', sans-serif;
+
+4. tailwindcss Для РЕАКТА своя установка
+
+https://tailwindcss.com/docs/guides/create-react-app
+
+1
+// npx create-react-app my-project
+// cd my-project
+
+2
+npm install -D tailwindcss
+npx tailwindcss init
+
+3
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+4. в ./src/index.css
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+5.
+npm run start
+
+6.
+export default function App() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    )
+  }
---
+++ client/src/pages/RegistPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RegistPage = () => {
+  return (
+    <div>RegistPage</div>
+  )
+}
