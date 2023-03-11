c4af74b5f373f72d23eb64f89eae8410032c234a
Aleksander <asava@sibmail.com>
2023-03-11 19:04:03 +0700

    add loginform

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,48 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { links } from '../data'
 
 
 export const Navbar = () => {
+    const isAuth = false
+    const activeStyles = {
+        color: 'white'
+    }
+
+    
   return (
     <div className='flex py-4 justify-between items-center '>
         <span className='flex justify-center items-center w-6 h-6 bg-gray-600 text-sm text-white rounded-sm'>E</span>
 
-        <ul className="flex gap-8">
-            <li>
+        
+
+        {
+            isAuth && <ul className="flex gap-8">
+            {
+                links.map(({name, path}, index) => {
+                    return (
+                        <li key={index}>
+                            <NavLink 
+                                to={path} 
+                                className='text-xs text-gray-500 hover:text-white'
+                                style={({ isActive }) => 
+                                    isActive ? activeStyles : undefined
+                    }
+                            >
+                                {name}
+                            </NavLink>
+                        </li>
+                    )
+                })
+            }
+            {/* <li>
                 <NavLink
                     to={'/'}
                     href="/"
-                    className='text-xs text-gray-100 hover:text-white'
+                    className='text-xs text-gray-500 hover:text-white'
+                    style={({ isActive }) => 
+                        isActive ? activeStyles : undefined
+                    }
                 >
                     Главня
                 </NavLink>
@@ -20,7 +50,10 @@ export const Navbar = () => {
             <li>
                 <NavLink
                     to={'/posts'}
-                    href="/" className='text-xs text-gray-100 hover:text-white'
+                    href="/" className='text-xs text-gray-500 hover:text-white'
+                    style={({ isActive }) => 
+                        isActive ? activeStyles : undefined
+                    }
                 >
                     Мои посты
                 </NavLink>
@@ -28,15 +61,20 @@ export const Navbar = () => {
             <li>
                 <NavLink
                     to={'/new'}
-                    href="/" className='text-xs text-gray-100 hover:text-white'
+                    href="/" className='text-xs text-gray-500 hover:text-white'
+                    style={({ isActive }) => 
+                        isActive ? activeStyles : undefined
+                    }
                 >
                     Добавит пост
                 </NavLink>
-            </li>
+            </li> */}
         </ul>
+        }
 
         <div className='flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2'>
-            <button>Войти</button>
+            {isAuth ? <button>Выйти</button> : <Link to={"/login"}>Войти</Link>}
+            
         </div>
     </div>
   )
---
+++ client/src/data.js
@@ -0,0 +1,28 @@
+
+
+export const links = [
+    {
+        name: "Главная",
+        path: '/'
+    },
+    {
+        name: "Мои посты",
+        path: '/posts'
+    },
+    {
+        name: "Добавить пост",
+        path: '/new'
+    },
+    // {
+    //     name: "Plans",
+    //     path: '/plans'
+    // },
+    // {
+    //     name: "Trainers",
+    //     path: '/trainers'
+    // },
+    // {
+    //     name: "Contact",
+    //     path: '/contact'
+    // }
+]
